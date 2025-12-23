196164155392489720e612951b238cc146282b0a
aleksey <kvyatkovsky89@gmail.com>
2025-12-23 12:28:06 +0300

    Refactored hello functions

--- go_learning_with_tests/1/hello_test.go
+++ go_learning_with_tests/1/hello_test.go
@@ -4,9 +4,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris!"
+	t.Run("saying hello to people in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("saying hello in French", func(t *testing.T) {
+		got := Hello("Madam", "French")
+		want := "Bonjour, Madam!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World', when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "English")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
--- go_learning_with_tests/1/main.go
+++ go_learning_with_tests/1/main.go
@@ -3,10 +3,35 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	english            = "English"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	prefix := greetingPrefix(language)
+	return fmt.Sprintf("%s%s!", prefix, name)
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", "English"))
 }
